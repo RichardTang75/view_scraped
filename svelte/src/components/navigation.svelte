9b75e7f0400a7a0fc990e54064a9acf507931f63
Richard Tang <richardtang75@gmail.com>
2022-10-04 01:31:46 -0400

    Remove one level of nesting for navigation

--- svelte/src/components/navigation.svelte
+++ svelte/src/components/navigation.svelte
@@ -5,16 +5,14 @@
 </script>
 
 <!-- TODO: solve unintuitive navigation bar -->
-<main>
-	<div>
-		{#if current_page > 2}
-			<button on:click={() => dispatch('switch_page', current_page - 2)}>{current_page - 2}</button>
-		{/if}
-		{#if current_page > 1}
-			<button on:click={() => dispatch('switch_page', current_page - 1)}>{current_page - 1}</button>
-        {/if}
-        <button on:click={() => dispatch('switch_page', current_page)}>{current_page}</button>
-        <button on:click={() => dispatch('switch_page', current_page + 1)}>{current_page + 1}</button>
-        <button on:click={() => dispatch('switch_page', current_page + 2)}>{current_page + 2}</button>
-	</div>
-</main>
+<div>
+    {#if current_page > 2}
+        <button on:click={() => dispatch('switch_page', current_page - 2)}>{current_page - 2}</button>
+    {/if}
+    {#if current_page > 1}
+        <button on:click={() => dispatch('switch_page', current_page - 1)}>{current_page - 1}</button>
+    {/if}
+    <button on:click={() => dispatch('switch_page', current_page)}>{current_page}</button>
+    <button on:click={() => dispatch('switch_page', current_page + 1)}>{current_page + 1}</button>
+    <button on:click={() => dispatch('switch_page', current_page + 2)}>{current_page + 2}</button>
+</div>
