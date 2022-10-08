eecbb4784b6268cc3e5564f9a3e49d9bb4028500
Richard Tang <richardtang75@gmail.com>
2022-10-08 17:00:17 -0400

    Fix navigation buttons moving

--- svelte/src/components/navigation.svelte
+++ svelte/src/components/navigation.svelte
@@ -4,16 +4,18 @@
     const dispatch = createEventDispatcher();
 </script>
 
-<!-- TODO: solve unintuitive navigation bar -->
 <div>
-    {#if current_page > 2}
-        <button on:click={() => dispatch('switch_page', current_page - 2)}>{current_page - 2}</button>
-    {/if}
-    {#if current_page > 1}
-        <button on:click={() => dispatch('switch_page', current_page - 1)}>{current_page - 1}</button>
-    {/if}
-    <button on:click={() => dispatch('switch_page', current_page)}>{current_page}</button>
+    <button class:hidden={current_page < 3} on:click={() => dispatch('switch_page', current_page - 2)}>{current_page - 2}</button>
+    <button class:hidden="{current_page < 2}" on:click={() => dispatch('switch_page', current_page - 1)}>{current_page - 1}</button>        
+    <button>{current_page}</button>
     <!-- TODO: use next and previous fields of query for this -->
     <button on:click={() => dispatch('switch_page', current_page + 1)}>{current_page + 1}</button>
     <button on:click={() => dispatch('switch_page', current_page + 2)}>{current_page + 2}</button>
 </div>
+
+
+<style>
+    .hidden {
+        visibility: hidden;
+    }
+</style>
