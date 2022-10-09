42fd256dc50004485bfdb3a3cc9624064543530a
Richard Tang <richardtang75@gmail.com>
2022-10-08 22:23:02 -0400

    Add permissions for ScrapedItem

--- api/filters.py
+++ api/filters.py
@@ -12,21 +12,3 @@ class ScrapedItemFilter(filters.FilterSet):
             'date': ['gte', 'lte'],
             'last_seen': ['gte', 'lte'],
         }
-
-# class ScrapedItemFilter(filters.FilterSet):
-#     link = filters.CharFilter(field_name='WantItem__link', lookup_expr='icontains', distinct=True, label='link')
-#     class Meta:
-#         model = ScrapedItem
-#         fields = ['WantItem__link']
-
-# class ScrapedItemFilter(filters.FilterSet):
-#     class Meta:
-#         model = ScrapedItem
-#         fields = {
-#             'title': ['icontains'],
-#             'description': ['icontains'],
-#             'link': ['icontains'],
-#             'date': ['gte', 'lte'],
-#             'last_seen': ['gte', 'lte'],
-#             'WantItem__link': ['icontains'],
-#         }
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import ScrapedItem, WantItem, IgnoreItem
 from .serializers import ScrapedItemSerializer, WantItemSerializer, IgnoreItemSerializer
 
@@ -8,15 +8,18 @@ from django_filters import rest_framework as filters
 from api.filters import ScrapedItemFilter
 
 class ItemList(generics.ListCreateAPIView):
-    # queryset = ScrapedItem.objects.all()
     queryset = ScrapedItem.objects.filter(wantitem__isnull=True).filter(ignoreitem__isnull=True).order_by('-date')
     serializer_class = ScrapedItemSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = ScrapedItemFilter
+    # items should be read only
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ScrapedItem.objects.all()
     serializer_class = ScrapedItemSerializer
+    # items should be read only
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class WantItemList(generics.ListCreateAPIView):
     queryset = WantItem.objects.all()
